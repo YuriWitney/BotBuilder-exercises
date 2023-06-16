0b5168abea689e241a788dc86874f6b386070767
Barros, Yuri <2165225@cognizant.com>
2023-06-16 12:34:56 -0300

    refactor: convert to interface

--- src/userProfile.ts
+++ src/userProfile.ts
@@ -1,8 +1,8 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-export class UserProfile {
-  public transport: string
-  public name: string
-  public age: number
+export interface UserProfile {
+  transport: string
+  name: string
+  age: number
 }
