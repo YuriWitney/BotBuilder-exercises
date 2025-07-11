163e4aea63baf60fa2336956691018223518ee82
Yuri Barros <scot39@gmail.com>
2025-07-11 00:03:50 -0300

    fix: remove token

--- release.config.js
+++ release.config.js
@@ -156,7 +156,6 @@ module.exports = {
     '(RC)+([0-9])?({+([0-9])})',
     '(sprint)+([0-9])?({+([0-9])})'
   ],
-  githubToken: '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',
   verifyConditions,
   analyzeCommits,
   verifyRelease,
